c884291d8d7f704857e48e7cbf01ec87ac288b8d
DanSeraf <seraf.daniele@gmail.com>
2019-01-18 17:44:41 +0100

    json check

--- fileop.py
+++ fileop.py
@@ -12,6 +12,13 @@ def loadJson():
         json_data = json.load(data_file)
         return json_data
 
+def jsonCheck():
+    try:
+        js = open('data.json', 'r')
+    except IOError:
+        js = open('data.json', 'w')
+        js.write("{}")
+
 def addLink(my_url):
     title = util.urlToTitle(my_url)
     json_data = loadJson()
@@ -43,5 +50,3 @@ def dataViewer():
     for key, value in json_data.items():
         i += 1
         print('[%d] - %s' %(i, value))
-
-
