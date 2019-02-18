761d09cb51a15989003000a2ae76fc97db18e742
DanSeraf <seraf.daniele@gmail.com>
2019-02-18 11:52:45 +0100

    return correct urls

--- search.py
+++ search.py
@@ -8,32 +8,29 @@ from util import removeDuplicated, urlToTitle, urlToTime
 
 query = 'http://www.youtube.com/watch?v='
 
-def generateUrl(query_str):
+def generateIds(query_str):
     query_string = urllib.parse.urlencode({"search_query" : query_str})
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
     return removeDuplicated(search_results)
 
 def singleUrl(query_str):
-    search_res = generateUrl(query_str)
+    search_res = generateIds(query_str)
     url = query + search_res[0]
     return url
 
 def multiUrl(query_str):
-    output = list
+    output = list()
     try:
-        uid_list = generateUrl(query_str)
+        uid_list = generateIds(query_str)
         urls = urlsExtractor(uid_list)
-        for key, url in urls.items():
-            print("[%s] - %s" %(key, url))
-            
+        for key, song in urls.items():
+            print("[%s] -> %s [%s]" %(key, song['title'], song['time']))
+
         uin = input('[*] Enter numbers of songs you want to add: ')
         udata = uin.split('-')
         for n in udata:
-            if (int(n) <= 10):
-                output.append(urls[int(n)])
-            else:
-                print('Error with input number')
+            output.append(urls[int(n)]['url'])
 
     except KeyboardInterrupt:
         pass
@@ -41,13 +38,13 @@ def multiUrl(query_str):
     return output 
 
 def urlsExtractor(uid_list, workers = 8):
-    def getTitle(uid):
+    def getSong(uid):
         url = query + uid
-        return urlToTitle(url) 
+        return {'url': url, 'title': urlToTitle(url), 'time': urlToTime(url)}
 
     with fut.ThreadPoolExecutor(workers) as executor:
         urls = {}
-        results = [executor.submit(getTitle, x) for x in uid_list]
+        results = [executor.submit(getSong, x) for x in uid_list]
         i = 0
         for res in fut.as_completed(results):
             i = i+1
